06ccdc9786c94a654095023c454e3f48f32f5e03
treesablossom-commits <treesablossom@gmail.com>
2026-08-25 15:20:11 +0530

    Update vln_ai_module_nano.launch.py

--- ai_module/src/vln_ai_module_nano/launch/vln_ai_module_nano.launch.py
+++ ai_module/src/vln_ai_module_nano/launch/vln_ai_module_nano.launch.py
@@ -1,1 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="vln_ai_module_nano",
+            executable="vln_ai_module_nano_node",
+            name="vln_ai_module_nano_node",
+            output="screen",
+        ),
+    ])
